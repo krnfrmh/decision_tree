6b8b9fe1a20941f8a050e68931cf43d4a442cccd
krnfrmh <87569650+krnfrmh@users.noreply.github.com>
2021-08-28 20:02:43 +0530

    Create TreeNode constructor

--- decision_tree_impl.py
+++ decision_tree_impl.py
@@ -10,3 +10,11 @@ def entropy(y):
     p0 = 1 - p1
     return -p0*np.log2(p0) - p1*np.log2(p1)
   
+    
+class TreeNode:
+    
+    def __init__(self, depth=1, max_depth=None):
+        self.depth = depth
+        self.max_depth = max_depth
+        if self.max_depth is not None and self.max_depth < self.depth:
+            raise Exception("depth > max_depth")
