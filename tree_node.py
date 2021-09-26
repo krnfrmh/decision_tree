666b76b59d3519b054ce8657d179edd5d97857aa
krnfrmh <87569650+krnfrmh@users.noreply.github.com>
2021-09-26 20:41:00 +0530

    Create Decision Tree Node

---
+++ tree_node.py
@@ -0,0 +1,111 @@
+import numpy as np
+
+class TreeNode:
+    
+    def __init__(self, depth=1, max_depth=None):
+        self.depth = depth
+        self.max_depth = max_depth
+        if self.max_depth is not None and self.max_depth < self.depth:
+            raise Exception("depth > max_depth")
+            
+    def fit(self, X, Y):
+        if len(Y) == 1 or len(set(Y)) == 1:
+            self.col = None
+            self.split = None
+            self.left = None
+            self.right = None
+            self.prediction = Y[0]
+        else:
+            D = X.shape[1]
+            cols = range(D)
+            max_ig = 0
+            best_col = None
+            best_split = None
+            for col in cols:
+                ig, split = self.find_split(X, Y, col)
+                if ig > max_ig:
+                    max_ig = ig
+                    best_col = col
+                    best_split = split
+            if max_ig == 0:
+                self.col = None
+                self.split = None
+                self.left = None
+                self.right = None
+                self.prediction = np.round(Y.mean())
+            else:
+                self.col = best_col
+                self.split = best_split
+
+                if self.depth == self.max_depth:
+                    self.left = None
+                    self.right = None
+                    self.prediction = [
+                        np.round(Y[X[:,best_col] < self.split].mean()),
+                        np.round(Y[X[:,best_col] >= self.split].mean())]
+                else:
+                    left_idx = (X[:,best_col] < best_split)
+                    Xleft = X[left_idx]
+                    Yleft = Y[left_idx]
+                    self.left = TreeNode(self.depth + 1, self.max_depth)
+                    self.left.fit(Xleft, Yleft)
+
+                    right_idx = (X[:,best_col] >= best_split)
+                    Xright = X[right_idx]
+                    Yright = Y[right_idx]
+                    self.right = TreeNode(self.depth + 1, self.max_depth)
+                    self.right.fit(Xright, Yright)
+            
+    def find_split(self, X, Y, col):
+        x_values = X[:, col]
+        sort_idx = np.argsort(x_values)
+        x_values = x_values[sort_idx]
+        y_values = Y[sort_idx]
+        
+        # boundaries between 2 classes or midpoint between 2 points
+        boundaries = np.nonzero(y_values[:-1] != y_values[1:])[0]
+        best_split = None
+        max_ig = 0
+        for b in boundaries:
+            split = (x_values[b] + x_values[b+1]) / 2
+            ig = self.information_gain(x_values, y_values, split)
+            if ig > max_ig:
+                max_ig = ig
+                best_split = split
+        return max_ig, best_split
+            
+    def information_gain(self, x, y, split):
+        y0 = y[x < split]
+        y1 = y[x >= split]
+        N = len(y)
+        y0len = len(y0)
+        if y0len == 0 or y0len == N:
+            return 0
+        p0 = float(len(y0)) / N
+        p1 = 1 - p0 #float(len(y1)) / N
+        return entropy(y) - p0*entropy(y0) - p1*entropy(y1)
+    
+    def predict_one(self, x):
+        if self.col is not None and self.split is not None:
+            feature = x[self.col]
+            if feature < self.split:
+                if self.left:
+                    p = self.left.predict_one(x)
+                else:
+                    p = self.prediction[0]
+            else:
+                if self.right:
+                    p = self.right.predict_one(x)
+                else:
+                    p = self.prediction[1]
+        else:
+            p = self.prediction
+        return p
+    
+    def predict(self, X):
+        N = len(X)
+        P = np.zeros(N)
+        for i in range(N):
+            P[i] = self.predict_one(X[i])
+        return P
+      
