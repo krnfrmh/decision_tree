a18414a39806e56f74320f1912ee176017f4319d
krnfrmh <87569650+krnfrmh@users.noreply.github.com>
2021-09-28 00:34:38 +0530

    Test Decision Tree on sample data

--- decision_tree_impl.py
+++ decision_tree_impl.py
@@ -1,4 +1,5 @@
 import numpy as np
+from decision_tree import DecisionTree
 
 def entropy(y):
     # assume binary classification problem
@@ -11,111 +12,30 @@ def entropy(y):
     return -p0*np.log2(p0) - p1*np.log2(p1)
   
     
-class TreeNode:
-    
-    def __init__(self, depth=1, max_depth=None):
-        self.depth = depth
-        self.max_depth = max_depth
-        if self.max_depth is not None and self.max_depth < self.depth:
-            raise Exception("depth > max_depth")
-            
-    def fit(self, X, Y):
-        if len(Y) == 1 or len(set(Y)) == 1:
-            self.col = None
-            self.split = None
-            self.left = None
-            self.right = None
-            self.prediction = Y[0]
-        else:
-            D = X.shape[1]
-            cols = range(D)
-            max_ig = 0
-            best_col = None
-            best_split = None
-            for col in cols:
-                ig, split = self.find_split(X, Y, col)
-                if ig > max_ig:
-                    max_ig = ig
-                    best_col = col
-                    best_split = split
-            if max_ig == 0:
-                self.col = None
-                self.split = None
-                self.left = None
-                self.right = None
-                self.prediction = np.round(Y.mean())
-            else:
-                self.col = best_col
-                self.split = best_split
+if __name__ == '__main__':
+    X, Y = get_data()
 
-                if self.depth == self.max_depth:
-                    self.left = None
-                    self.right = None
-                    self.prediction = [
-                        np.round(Y[X[:,best_col] < self.split].mean()),
-                        np.round(Y[X[:,best_col] >= self.split].mean())]
-                else:
-                    left_idx = (X[:,best_col] < best_split)
-                    Xleft = X[left_idx]
-                    Yleft = Y[left_idx]
-                    self.left = TreeNode(self.depth + 1, self.max_depth)
-                    self.left.fit(Xleft, Yleft)
+    # only take 0s and 1s since we're doing binary classification
+    idx = np.logical_or(Y == 0, Y == 1)
+    X = X[idx]
+    Y = Y[idx]
 
-                    right_idx = (X[:,best_col] >= best_split)
-                    Xright = X[right_idx]
-                    Yright = Y[right_idx]
-                    self.right = TreeNode(self.depth + 1, self.max_depth)
-                    self.right.fit(Xright, Yright)
-            
-    def find_split(self, X, Y, col):
-        x_values = X[:, col]
-        sort_idx = np.argsort(x_values)
-        x_values = x_values[sort_idx]
-        y_values = Y[sort_idx]
-        
-        # boundaries between 2 classes or midpoint between 2 points
-        boundaries = np.nonzero(y_values[:-1] != y_values[1:])[0]
-        best_split = None
-        max_ig = 0
-        for b in boundaries:
-            split = (x_values[b] + x_values[b+1]) / 2
-            ig = self.information_gain(x_values, y_values, split)
-            if ig > max_ig:
-                max_ig = ig
-                best_split = split
-        return max_ig, best_split
-            
-    def information_gain(self, x, y, split):
-        y0 = y[x < split]
-        y1 = y[x >= split]
-        N = len(y)
-        y0len = len(y0)
-        if y0len == 0 or y0len == N:
-            return 0
-        p0 = float(len(y0)) / N
-        p1 = 1 - p0 #float(len(y1)) / N
-        return entropy(y) - p0*entropy(y0) - p1*entropy(y1)
+    # split the data
+    Ntrain = len(Y) // 2
+    Xtrain, Ytrain = X[:Ntrain], Y[:Ntrain]
+    Xtest, Ytest = X[Ntrain:], Y[Ntrain:]
     
-    def predict_one(self, x):
-        if self.col is not None and self.split is not None:
-            feature = x[self.col]
-            if feature < self.split:
-                if self.left:
-                    p = self.left.predict_one(x)
-                else:
-                    p = self.prediction[0]
-            else:
-                if self.right:
-                    p = self.right.predict_one(x)
-                else:
-                    p = self.prediction[1]
-        else:
-            p = self.prediction
-        return p
+    model = DecisionTree()
+    # model = DecisionTree(max_depth=7)
+    t0 = datetime.now()
+    model.fit(Xtrain, Ytrain)
+    print("Training time:", (datetime.now() - t0))
+
+    t0 = datetime.now()
+    print("Train accuracy:", model.score(Xtrain, Ytrain))
+    print("Time to compute train accuracy:", (datetime.now() - t0))
+
+    t0 = datetime.now()
+    print("Test accuracy:", model.score(Xtest, Ytest))
+    print("Time to compute test accuracy:", (datetime.now() - t0))
     
-    def predict(self, X):
-        N = len(X)
-        P = np.zeros(N)
-        for i in range(N):
-            P[i] = self.predict_one(X[i])
-        return P
